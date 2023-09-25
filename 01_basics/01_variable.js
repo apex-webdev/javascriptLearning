74a997a14ca1fa41dd044d897a5072b7e7ed4ff6
nowshad-apexwedev <143169786+nowshad-apexwedev@users.noreply.github.com>
2023-09-25 09:01:27 +0000

    datatypes in Js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,4 @@
+const accountId = 234224
+let accountEmail = "nowshadislam02@gmail.com"
+ accountCity = "dhaka" //auto declared
+console.table([accountId, accountEmail, accountCity]) 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+// ss bb nn u
+// string symbol     boolean bigInt     number null     undefined >>> primitive data types
+//Object >>> non-primitive data types
+let variable_1; // type u=Undefined
+let score = null;// type Object
+console.table([typeof(variable_1), typeof(null)])
