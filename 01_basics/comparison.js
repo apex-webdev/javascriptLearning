d25549c7285e69d07ca53c79e18affbc0458a3c1
nowshad-apexwedev <143169786+nowshad-apexwedev@users.noreply.github.com>
2023-09-25 09:49:08 +0000

    comparison operators confusion in Js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+/* Comparison operators( <, >, <=, >=) adn equality check(==) works differently in Js. Comparison operators converts can convert other data types to number. */
