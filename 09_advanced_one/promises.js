1154050181503976a0261e0fd530c4ea5efa81f9
Nowshad <143169786+nowshad-apexwedev@users.noreply.github.com>
2023-10-11 08:54:07 +0000

    updated promises

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -48,6 +48,50 @@ promiseFour
   .catch(function (error) {
     console.log(error);
   })
-   .finally(() => {
-    console.log("The promise is either resolved or rejected")
-   })
+  .finally(() => {
+    console.log("The promise is either resolved or rejected");
+  });
+
+const promiseFive = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    let error = 1;
+    if (!error) {
+      resolve({ language: "javascript", pin: 2521 });
+    } else {
+      reject("ERROR: Js went wrong.");
+    }
+  }, 1000);
+});
+/*async function consumepromiseFive() {
+  const response = await promiseFive;
+  console.log(response);
+}
+consumepromiseFive();*/
+async function consumepromiseFiveAlt(){
+  try{
+    const response = await promiseFive
+    console.log(response)
+  }catch(error){
+    console.log(error)
+  }
+}
+consumepromiseFiveAlt()
+
+/*async function getUserData(){
+  try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+  } catch(error){
+    console.log('Error 404!')
+  }
+}
+getUserData()*/
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+  return response.json()
+}).then((data)=>{
+  console.log(data)
+}).catch((error)=>console.log(error))
+
